06a9b98f851d3b928d1fa50126b5e91fb9058c1a
Rotaru Andrei <rotarua99@gmail.com>
2026-05-11 02:07:32 +0300

    initial complete project

--- obj/Debug/net10.0/Proiect SO.AssemblyInfo.cs
+++ obj/Debug/net10.0/Proiect SO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proiect SO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcc26455db219e2919fdcef02d93100b026808c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proiect SO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proiect SO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
